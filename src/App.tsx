0cdd609d9c078e028f5b6e891021aa17a1d741cf
Marco Tettamanti <marcotettamanti@outlook.com.br>
2022-09-26 08:15:55 -0300

    adicionar interações

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
 import style from './App.module.css'
 import iconLogo from './assets/calculator.png'
 import iconGithub from './assets/github.png'
-import { useState } from 'react'
+import { MouseEventHandler, useState } from 'react'
 import { buttonsCalculator} from './helpers/calculator'
 import { GridCalculator } from './components/GridItem'
 
 
 const App = () => {
-  let [calculatorString , setCalculatorString] = useState<string>('')
+  let [calculatorHistory , setCalculatorHistory] = useState<string>('')
   let [calculatorTotal , setCalculatorTotal] = useState<number>(0)
   
-  
+  const addInput = (input:any) => {
+    setCalculatorHistory(calculatorHistory.concat(input))
+  }
+
   return(
     <div className={style.main}>
       
       <div className={style.headerContainer}>
-        <img src={iconLogo} alt="" width={25} title='Github ícones criados por Freepik - Flaticon'/>
+        <img src={iconLogo} alt="" width={25} title='G - Flaticon'/>
         <h2 className={style.titleMain}>Calculadora Simples</h2>
       </div>
     
       <section className={style.displayCalculator}>
         <div className={style.displayTop}>
-          <div className={style.historic}>{calculatorString}</div>
+          <div className={style.historic}>{calculatorHistory}</div>
           <div className={style.result}>{calculatorTotal}</div>
         </div>
 
         <div className={style.displayBottom}>
           <div className={style.containerDisplay}>
-            {buttonsCalculator.map((item , key) => (
-              <GridCalculator key={key} item={item}/>
+
+            {buttonsCalculator.map((item , key , onClick) => (
+              <GridCalculator 
+                key={key} 
+                item={item}
+                onClick={e => {addInput(e.target.value)}} />
             ))}
+            
           </div>
         </div>
       </section>
--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,16 +1,19 @@
-import { Buttons } from '../helpers/calculator';
+import { Buttons, buttonsCalculator } from '../helpers/calculator';
 import style from './styles/GridItem.module.css'
 
 type Props = {
-  item: Buttons
+  item: Buttons;
+  onClick: (e:MouseEvent<HTMLInputElement>) => void
 }
 
-export const GridCalculator = ({ item }:Props) => {
+export const GridCalculator = ({ item , onClick}:Props) => {
   return (
     <div className={style.main}>
-      <input className={style.buttonsNumber} type="button" value={item.button} 
-
-      style={{backgroundImage: `linear-gradient(${item.color[0]},${item.color[1]})`}}
+      <input
+        onClick={e => onClick(e)}
+        className={style.buttonsNumber}
+        type="button" value={item.button}
+        style={{backgroundImage: `linear-gradient(${item.color[0]},${item.color[1]})`}}
       />
     </div>
   )
